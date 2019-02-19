2626558c1c6f7b4c94405074baa963a1677f13ff
enemy <nkalmykov937@gmail.com>
2019-02-19 20:59:02 +0300

    Выполнил задание номер 1

--- HomeWorkLesson8/mission1/Form1.Designer.cs
+++ HomeWorkLesson8/mission1/Form1.Designer.cs
@@ -28,13 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.bttnRef = new System.Windows.Forms.Button();
+            this.TbForRef = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // bttnRef
+            // 
+            this.bttnRef.Location = new System.Drawing.Point(12, 311);
+            this.bttnRef.Name = "bttnRef";
+            this.bttnRef.Size = new System.Drawing.Size(89, 23);
+            this.bttnRef.TabIndex = 0;
+            this.bttnRef.Text = "Показать тип";
+            this.bttnRef.UseVisualStyleBackColor = true;
+            this.bttnRef.Click += new System.EventHandler(this.bttnRef_Click);
+            // 
+            // TbForRef
+            // 
+            this.TbForRef.Location = new System.Drawing.Point(12, 12);
+            this.TbForRef.Multiline = true;
+            this.TbForRef.Name = "TbForRef";
+            this.TbForRef.Size = new System.Drawing.Size(776, 281);
+            this.TbForRef.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.TbForRef);
+            this.Controls.Add(this.bttnRef);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button bttnRef;
+        private System.Windows.Forms.TextBox TbForRef;
     }
 }
 
